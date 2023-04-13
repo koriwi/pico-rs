08ca9a86227c881925ac6904224b5d0c259ea485
Kilian Gosewisch <kilian2798@gmail.com>
2023-04-14 00:31:24 +0200

    clean up

--- src/main.rs
+++ src/main.rs
@@ -13,59 +13,24 @@
 #![no_main]
 extern crate alloc;
 mod button_machine;
-use alloc::boxed::Box;
+mod mux;
 use defmt::{debug, println};
-use embedded_hal::digital::v2::{InputPin, OutputPin};
+use rp_pico::{hal, Pins};
+
+use alloc::boxed::Box;
 // The macro for our start-up function
+use cortex_m_rt::entry;
 use defmt_rtt as _;
-use fugit::{Instant, RateExtU32};
-use rp_pico::{
-    entry,
-    hal::{
-        gpio::{DynInput, DynPin, DynPinId, DynPinMode},
-        uart::{DataBits, StopBits, UartConfig},
-        Clock,
-    },
-    Pins,
-};
+use hal::{gpio::DynPin, pac, Clock};
+
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
-use panic_halt as _;
-
-// A shorter alias for the Peripheral Access Crate, which provides low-level
-// register access
-use rp_pico::hal::pac;
-
-// A shorter alias for the Hardware Abstraction Layer, which provides
-// higher-level drivers.
-use rp_pico::hal;
-
-// USB Device support
-use usb_device::{class_prelude::*, prelude::*};
-
-// USB Communications Class Device support
-use usbd_serial::SerialPort;
-
-// Used to demonstrate writing formatted strings
 use alloc_cortex_m::CortexMHeap;
-use core::fmt::Write;
-use heapless::String;
+use panic_probe as _;
+
+use crate::mux::{get_mux_pins, set_mux_addr};
 #[global_allocator]
 static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
-
-fn get_mux_addr_bits(addr: u8) -> (bool, bool, bool) {
-    let mut addr_bits = (false, false, false);
-    if addr & 0b0001 != 0 {
-        addr_bits.0 = true;
-    }
-    if addr & 0b0010 != 0 {
-        addr_bits.1 = true;
-    }
-    if addr & 0b0100 != 0 {
-        addr_bits.2 = true;
-    }
-    addr_bits
-}
 
 fn create_pin_array(pins: Pins) -> [Option<DynPin>; 29] {
     let pin_array: [Option<DynPin>; 29] = [
@@ -111,15 +76,16 @@ fn create_pin_array(pins: Pins) -> [Option<DynPin>; 29] {
 /// received over USB Serial.
 #[entry]
 fn main() -> ! {
-    // Grab our singleton objects
+    unsafe {
+        ALLOCATOR.init(cortex_m_rt::heap_start() as usize, 1024);
+    }
+    println!("free memory: {}", ALLOCATOR.free());
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
 
     // Set up the watchdog driver - needed by the clock setup code
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
-    // Configure the clocks
-    //
     // The default is to generate a 125 MHz system clock
     let clocks = hal::clocks::init_clocks_and_plls(
         rp_pico::XOSC_CRYSTAL_FREQ,
@@ -143,52 +109,12 @@ fn main() -> ! {
 
     let mut pin_array = create_pin_array(pins);
 
-    // Set up the USB driver
-    // let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
-    //     pac.USBCTRL_REGS,
-    //     pac.USBCTRL_DPRAM,
-    //     clocks.usb_clock,
-    //     true,
-    //     &mut pac.RESETS,
-    // ));
-
-    // Set up the USB Communications Class Device driver
-    // let mut serial = SerialPort::new(&usb_bus);
-    // Create a USB device with a fake VID and PID
-    // let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-    //     .manufacturer("Fake company")
-    //     .product("Serial port")
-    //     .serial_number("TEST")
-    //     .device_class(2) // from: https://www.usb.org/defined-class-codes
-    //     .build();
-
     // get pin programmatically by index
-    let mut mux_s0 = pin_array[20].take().unwrap();
-    let mut mux_s1 = pin_array[21].take().unwrap();
-    let mut mux_s2 = pin_array[22].take().unwrap();
+    let mut mux_pins = get_mux_pins(&mut pin_array);
     let mut button_pin = pin_array[19].take().unwrap();
-
-    mux_s0.into_push_pull_output();
-    mux_s1.into_push_pull_output();
-    mux_s2.into_push_pull_output();
     button_pin.into_pull_up_input();
 
-    let bits = get_mux_addr_bits(0);
-    if bits.0 {
-        mux_s0.set_high().unwrap();
-    } else {
-        mux_s0.set_low().unwrap();
-    }
-    if bits.1 {
-        mux_s1.set_high().unwrap();
-    } else {
-        mux_s1.set_low().unwrap();
-    }
-    if bits.2 {
-        mux_s2.set_high().unwrap();
-    } else {
-        mux_s2.set_low().unwrap();
-    }
+    set_mux_addr(0, &mut mux_pins);
 
     let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
     let mut button_index = 0;
@@ -200,7 +126,7 @@ fn main() -> ! {
             debug!("action: {}: {:?}", index, action);
         }),
     );
-
+    println!("free memory: {}", ALLOCATOR.free());
     loop {
         if timer.get_counter().ticks() % 10000 == 0 {
             button_machine.check_button(button_index, true).unwrap();
@@ -208,73 +134,12 @@ fn main() -> ! {
             if button_index > 7 {
                 button_index = 0;
             }
-            let bits = get_mux_addr_bits(button_index);
-            if bits.0 {
-                mux_s0.set_high().unwrap();
-            } else {
-                mux_s0.set_low().unwrap();
-            }
-            if bits.1 {
-                mux_s1.set_high().unwrap();
-            } else {
-                mux_s1.set_low().unwrap();
-            }
-            if bits.2 {
-                mux_s2.set_high().unwrap();
-            } else {
-                mux_s2.set_low().unwrap();
-            }
+            set_mux_addr(button_index, &mut mux_pins);
         }
         // if serial.line_coding().data_rate() == 1200 {
         //     // Reset the board if the host sets the baud rate to 1200
         //     hal::rom_data::reset_to_usb_boot(0, 0);
         // }
         // usb_dev.poll(&mut [&mut serial]);
-
-        // // A welcome message at the beginning
-        // if !said_hello && timer.get_counter().ticks() >= 2_000_000 {
-        //     said_hello = true;
-        //     let _ = serial.write(b"Hello, World!\r\n");
-
-        //     let time = timer.get_counter().ticks();
-        //     let mut text: String<64> = String::new();
-        //     writeln!(&mut text, "Current timer ticks: {}", time).unwrap();
-
-        //     // This only works reliably because the number of bytes written to
-        //     // the serial port is smaller than the buffers available to the USB
-        //     // peripheral. In general, the return value should be handled, so that
-        //     // bytes not transferred yet don't get lost.
-        //     let _ = serial.write(text.as_bytes());
-        // }
-
-        // // Check for new data
-        // if usb_dev.poll(&mut [&mut serial]) {
-        //     let mut buf = [0u8; 64];
-        //     match serial.read(&mut buf) {
-        //         Err(_e) => {
-        //             // Do nothing
-        //         }
-        //         Ok(0) => {
-        //             // Do nothing
-        //         }
-        //         Ok(count) => {
-        //             // Convert to upper case
-        //             buf.iter_mut().take(count).for_each(|b| {
-        //                 b.make_ascii_uppercase();
-        //             });
-        //             // Send back to the host
-        //             let mut wr_ptr = &buf[..count];
-        //             while !wr_ptr.is_empty() {
-        //                 match serial.write(wr_ptr) {
-        //                     Ok(len) => wr_ptr = &wr_ptr[len..],
-        //                     // On error, just drop unwritten data.
-        //                     // One possible error is Err(WouldBlock), meaning the USB
-        //                     // write buffer is full.
-        //                     Err(_) => break,
-        //                 };
-        //             }
-        //         }
-        //     }
-        // }
     }
 }
---
+++ src/mux.rs
@@ -0,0 +1,25 @@
+use embedded_hal::digital::v2::OutputPin;
+use rp_pico::hal;
+
+use alloc::vec::Vec;
+use hal::gpio::DynPin;
+
+// todo: read the pin range from sdcard
+pub fn get_mux_pins(pin_array: &mut [Option<DynPin>; 29]) -> Vec<DynPin> {
+    let mut mux_pins = Vec::with_capacity(3);
+    for i in 20..23 {
+        let mut pin = pin_array[i].take().unwrap();
+        pin.into_push_pull_output();
+        mux_pins.push(pin);
+    }
+    mux_pins
+}
+
+pub fn set_mux_addr(addr: u8, mux_pins: &mut [DynPin]) {
+    for (index, pin) in mux_pins.iter_mut().enumerate() {
+        match addr & (1 << index) {
+            0 => pin.set_low().unwrap(),
+            _ => pin.set_high().unwrap(),
+        }
+    }
+}
